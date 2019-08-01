325e4a175c68c9e9bc1cbb36a66430ed89ea43f2
Nicole Alvarez <ngalvare@hotmai.com>
2019-07-31 20:59:08 -0500

    Se modifico Manejador y su interfaz

--- TallerIntegradorPatrones/src/Patrones/ManejadorDinero.java
+++ TallerIntegradorPatrones/src/Patrones/ManejadorDinero.java
@@ -1,62 +1,63 @@
- /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package Patrones;
 
-public class ManejadorDinero implements Manejador
-{
+public class ManejadorDinero implements Manejador {
+
     protected int monto;
     protected double denominacion;
     private Manejador next;
 
-    public ManejadorDinero(int monto, double denominacion){
+    public ManejadorDinero(int monto, double denominacion) {
         this.monto = monto; // Total de billetes
         this.denominacion = denominacion; // Valor de cada billete
         this.next = null;
     }
 
     @Override
-    public int getMonto(){ return monto; }
-    @Override
-    public double getDenominacion(){ return denominacion; }
-    public void setMonto(int monto){ this.monto = monto; }
-    
-     @Override
-    public Manejador crearManejador(int n, double denominacion) {
-        return new ManejadorDinero(n,denominacion);
+    public int getMonto() {
+        return monto;
     }
 
     @Override
-    public boolean retirar(int monto){
-        if(monto==0) return true;
-        if(monto>=denominacion && this.monto>0 ){
-           monto-= denominacion;
-           this.monto--;
-          return retirar(monto);
+    public double getDenominacion() {
+        return denominacion;
+    }
+
+    public void setMonto(int monto) {
+        this.monto = monto;
+    }
+
+    @Override
+    public boolean retirar(int monto) {
+        if (monto == 0) {
+            return true;
         }
-        
-        else{
-            if(next!=null){
+        if (monto >= denominacion && this.monto > 0) {
+            monto -= denominacion;
+            this.monto--;
+            return retirar(monto);
+        } else {
+            if (next != null) {
                 return next.retirar(monto);
-            }
-            else{
+            } else {
                 return false;
             }
         }
-        }
-    
+    }
+
     @Override
-    public boolean depositar(int cantidad, double denominacion){
-        if(denominacion==this.denominacion){
-            this.monto=(int) (this.monto + (cantidad/denominacion));
+    public boolean depositar(int cantidad, double denominacion) {
+        if (denominacion == this.denominacion) {
+            this.monto = (int) (this.monto + (cantidad / denominacion));
             return true;
-        }
-        else{ 
-            if(next!=null){
-                return next.depositar(cantidad, denominacion);}
-            else {
+        } else {
+            if (next != null) {
+                return next.depositar(cantidad, denominacion);
+            } else {
                 return false;
             }
         }
@@ -64,7 +65,7 @@ public class ManejadorDinero implements Manejador
 
     @Override
     public void setNext(Manejador manejador) {
-        this.next=manejador; //Setea el el viejo manejador por uno nuevo
+        this.next = manejador; //Setea el el viejo manejador por uno nuevo
     }
 
     @Override
@@ -72,5 +73,4 @@ public class ManejadorDinero implements Manejador
         return this.next;
     }
 
-  
 }
