c7ba330534f2cb1f52b138e42356bd88daa47e2f
Nicole Alvarez <ngalvare@hotmai.com>
2019-08-01 23:23:23 -0500

    Se agrego CuentaAdapter

---
+++ TallerIntegradorPatrones/src/Patrones/CuentaAdapter.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Patrones;
+
+import java.util.Currency;
+
+/**
+ *
+ * @author Nicole Alvarez
+ */
+public class CuentaAdapter implements Cuenta{
+    protected Account cuenta;
+    protected  Currency moneda;
+
+    public CuentaAdapter(int id, double amount) {
+         cuenta = new Account(id,amount);
+        
+    }
+
+    @Override
+    public double Balance() {
+        return cuenta.getAmount();
+    }
+
+    @Override
+    public boolean Retirar(double monto) {
+        if(cuenta.withdraw(monto).startsWith("Error:"))
+                return true;
+        return false;
+    }
+
+    @Override
+    public boolean Depositar(int n, double denominacion) {
+        double total = n*denominacion;
+        cuenta.deposit(total);
+        return true;
+    }
+
+    @Override
+    public int cuentaID() {
+        return cuenta.getId();
+    }
+    
+}
